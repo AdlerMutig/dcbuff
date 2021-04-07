8cdc76461239a62e08d40491b1667c6d12c53c32
PaoloAgVa <paoloagva@gmail.com>
2021-04-08 01:04:34 +0200

    Uses two microservices on different ports

---
+++ src/main/java/com/dcbuff/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.dcbuff.user.repository;
+
+import com.dcbuff.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    User findByUserId(Long userId);
+}
